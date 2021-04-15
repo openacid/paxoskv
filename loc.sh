95a8315553b4516a91b321e5aa55801874c7941e
Restyled.io <commits@restyled.io>
2021-04-15 04:31:10 +0000

    Restyled by shellharden

--- loc.sh
+++ loc.sh
@@ -5,11 +5,11 @@ fns=$(find . -name "*.go" \
     | grep -v "\.pb\.go" \
     | grep -v "_test.go")
 
-echo fns: $fns
+echo fns: "$fns"
 
 # fns=./paxoskv/impl.go
 
-cat $fns \
+cat "$fns" \
     | grep -v "^	*//" \
     | grep -v "^$" \
     | grep -v "pretty\." \
